348c1740b36e07f4e1620a572819aaf43cddb4d2
Delaplacette <delaplacette_didier@yahoo.fr>
2024-07-15 13:36:35 +0200

    F03 : Fruit names modification

--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Fruit() {
   return (
-<h1>reprise</h1>  )
+    <>
+      <h1>reprise</h1>
+
+      <div>
+        <p>marcel</p>
+        <h1>marc</h1>
+        <p>marcs</p>
+        <p>gg</p>
+      </div>
+    </>
+  );
 }
